dc4ee3918ad4a1273ee7a0d826aa0f3c60ca3827
EverSoJoe <42702674+EverSoJoe@users.noreply.github.com>
2022-09-13 07:42:47 +0200

    made install script more reliable

--- install.bash
+++ install.bash
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+ABSOLUTE="$( readlink -f -- "$0" )"
+DIRECTORY="$( dirname "$ABSOLUTE" )"
+cd "$DIRECTORY"
+
 find . -type d -exec chmod 700 {} \;
 find . -type f -exec chmod 600 {} \;
 
 shopt -s dotglob
 shopt -s extglob
 yes | cp -rf ./!(install.bash|.git) ~
-
+rm -rf "$DIRECTORY"
